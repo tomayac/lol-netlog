2f06a032eae5e65c27c95e8d61ec518cfb5c4cc9
Nelson Minar <nelson@monkey.org>
2014-06-04 17:39:13 -0700

    add fonts and background image

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 set -eu
-rsync -avz --delete \
-    webapp/* \
-    somebits.com:/var/www/lol-netlog/
-rsync -avzR --delete server logToJson.js webapp/lol-netlog-parse.js somebits.com:/home/nelson/lol-netlog
 
+rsync -avz --delete \
+    webapp/{bg.jpg,*.html,*.js} \
+    somebits.com:/var/www/lol-netlog/
+
+rsync -avzR --delete \
+    server logToJson.js webapp/lol-netlog-parse.js \
+    somebits.com:/home/nelson/lol-netlog
